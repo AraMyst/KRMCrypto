3ddd230c5191c28cac3acd4cf04c35fdb7eae41b
Karla de Matos <kcristinardematos@gmail.com>
2025-06-09 13:49:17 +0200

    deploy 7

--- frontend-en/src/pages/_document.tsx
+++ frontend-en/src/pages/_document.tsx
@@ -8,7 +8,7 @@ import Document, {
   DocumentInitialProps
 } from 'next/document';
 
-export default class MyDocument extends Document<{}, {}, {}> {
+export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
@@ -23,14 +23,17 @@ export default class MyDocument extends Document<{}, {}, {}> {
           {/* Meta tags essenciais */}
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#1A73E8" />
-          {/* Exemplo: fonte customizada via Google Fonts */}
+          {/* Fontes via Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          {/* Aqui você pode adicionar analytics, favicon alternativo, etc. */}
         </Head>
         <body className="antialiased bg-white text-gray-900 font-sans">
           <Main />
